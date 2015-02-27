2fe4d1abe9e1ee6e0717eeeb5d25a318b7080afd
Gabi Huiber <ghuiber@gmail.com>
2015-02-27 00:23:22 -0500

    Got my better pictures!
    Zelig 5 breakthrough

--- amash.R
+++ amash.R
@@ -5,9 +5,10 @@
 # Complete data : http://bit.ly/11IhEtY
 # -- amashbig.csv
 rm(list=ls(all=TRUE))
-library(coin)
-library(Zelig)
-library(ggplot2)
+library("coin")
+library("Zelig")
+library("ggplot2")
+library("dplyr")
 
 # Read raw data
 df <- read.csv('amashvotevsmoney.csv',header=T,sep="|",stringsAsFactors=F,strip.white=T)
@@ -19,6 +20,13 @@ names(df)[3] <- 'District' # shorten district name
 df   <- df[df$Vote!='Not Voting',] # drop abstainers (want 2 categories for Wilcoxon rank-sum test)
 incr <- 5000
 maxd <- 200000
+rng  <- seq(0,maxd,incr)
+
+# Colorblind-safe red/blue hues for 
+# Republican/Democrat lines
+myRedBlue        <- c("#ef8a62", "#67a9cf")
+parties          <- c('R','D') # in this order
+names(myRedBlue) <- parties
 
 # Relative average prices: how much for a No for Democrats vs. Republicans, nationally
 getGoingPrice <- function(vote) {
@@ -38,12 +46,12 @@ getGoingPrice <- function(vote) {
 
 # How much for a No vs. Yes within a party?
 getDifference <- function(party) {
-   require(coin)
+   require("coin")
    d <- df[df$Party==party,]
    yes   <- d[d$Vote=='Yes',]
    no    <- d[d$Vote=='No',]
-   yes.r <- c(dim(yes)[1],mean(yes$Amount),sd(yes$Amount))
-   no.r  <- c(dim(no)[1],mean(no$Amount),sd(no$Amount))
+   yes.r <- c(nrow(yes),mean(yes$Amount),sd(yes$Amount))
+   no.r  <- c(nrow(no),mean(no$Amount),sd(no$Amount))
    tab  <- rbind(yes.r,no.r)
    colnames(tab) <- c("Votes","Mean","St.dev")
    rownames(tab) <- paste(party,c("Yes","No"),sep=".")
@@ -58,8 +66,8 @@ getDifference <- function(party) {
 getAmount <- function(vote,party,state) {
    big <- df[df$Vote==vote & df$Party==party,]
    lil <- big[grep(state,big$District),]
-   a <- c(mean(big$Amount, na.rm=T),median(big$Amount, na.rm=T),sd(big$Amount, na.rm=T),dim(big)[1])
-   b <- c(mean(lil$Amount, na.rm=T),median(lil$Amount, na.rm=T),sd(lil$Amount, na.rm=T),dim(lil)[1])
+   a <- c(mean(big$Amount, na.rm=T),median(big$Amount, na.rm=T),sd(big$Amount, na.rm=T),nrow(big))
+   b <- c(mean(lil$Amount, na.rm=T),median(lil$Amount, na.rm=T),sd(lil$Amount, na.rm=T),nrow(lil))
    d <- rbind(a,b)
    rownames(d) <- c("US",state)
    colnames(d) <- c("Mean","Median","SD",paste(party,vote,sep="."))
@@ -85,110 +93,100 @@ NC.R.No  <- getAmount('No','R','NC')
 print(format(NC.R.No$summary,big.mark=",",digits=4),quote=F)
 print(format(No$summary,big.mark=",",digits=4),quote=F)
 
-# Now estimate prob of voting No as a function of Party and Amount. Use Zelig.
+# Now estimate prob of voting No as a function of Party and Amount.
+# Use Zelig 5 with Zelig 4 compatibility wrappers.
 df$Vote  <- factor(df$Vote, levels=c('Yes','No'))
 df$Party <- factor(df$Party)
-z1.out <- zelig(Vote ~ Party + Amount, data=df, model="logit", cite=FALSE)
-z2.out <- zelig(Vote ~ Party + Amount + Party*Amount, data=df, model="logit", cite=FALSE)
-getSims <- function(z.out) {
-   x.R <- setx(z.out,Party='R',Amount=seq(0,maxd,incr))
-   x.D <- setx(z.out,Party='D',Amount=seq(0,maxd,incr))
-   s.out <- sim(z.out,x=x.D,x1=x.R)
-   s.D <- sim(z.out,x=x.D,x1=x.R)
-   s.R <- sim(z.out,x=x.R,x1=x.D)
-   c <- list(x.R,x.D,s.out,s.D,s.R)
-   names(c) <- c('x.R','x.D','s.out','s.D','s.R')
-   return(c)
-}   
-m1 <- getSims(z1.out)
-m2 <- getSims(z2.out)
 
-# Collect predicted values from a Zelig sim() object
-collectPredictedValues <- function(zs) {
-   rnames    <- names(summary(zs)[['stats']])
-   expected  <- NULL
-   predicted <- NULL
-   for(i in rnames) {
-      expected  <- rbind(expected,summary(zs)[['stats']][[i]][[1]])
-      predicted <- rbind(predicted,summary(zs)[['stats']][[i]][[2]])
-   }
-   c <- cbind(expected,predicted)
-   rownames(c) <- rnames
-   return(c)
+# Pure Zelig 5
+z1 <- zlogit$new()
+z1$zelig(Vote ~ Party + Amount, data=df)
+z1$setrange(Party='R', Amount=rng)
+z1$setrange1(Party='D', Amount=rng)
+z1$sim()
+plot.ci(z1, ci=95)
+
+z2 <- zlogit$new()
+z2$zelig(Vote ~ Party * Amount, data=df)
+z2$setrange(Party='R', Amount=rng)
+z2$setrange1(Party='D', Amount=rng)
+z2$sim()
+plot.ci(z2, ci=95)
+
+m1 <- z1$sim.out
+m2 <- z2$sim.out
+
+#' Pick out data for drawing ggplot2 version of plot.ci().
+#' Need a series of of median expected values bracketed by
+#' the bottom and the top of a confidence interval, ci.
+#' @param x a zlogit$sim.out object with range and range1
+#' @param ci width of the confidence interval
+getRibbon <- function(x,ci=.95) {
+  ran       <- x$range  
+  ran1      <- x$range1
+  n         <- length(rng)  
+  # get bottom 2.5%th, top 97.5%th, median
+  getLoMeHi <- function(x) {
+    ci  <- (1-ci)/2
+    ve  <- sort(x$ev[[1]])
+    lo  <- floor(length(ve)*ci)
+    hi  <- ceiling(length(ve)*(1-ci))
+    lo  <- ve[lo]
+    hi  <- ve[hi]
+    me  <- median(ve)
+    return(c(lo,me,hi))
+  }
+  evmatrix  <- matrix(rep(NA,n*3),ncol=3)
+  evmatrix1 <- evmatrix
+  for(i in 1:n) {
+    evmatrix[i,]  <- getLoMeHi(ran[[i]])
+    evmatrix1[i,] <- getLoMeHi(ran1[[i]])
+  }
+  evmatrix  <- cbind(0L,rng,evmatrix)
+  evmatrix1 <- cbind(1L,rng,evmatrix1)
+  out <- rbind(evmatrix,evmatrix1)
+  # Now make it useful for ggplot2
+  colnames(out) <- c('Party','Amount','Low','Median','High')
+  out <- as.data.frame(out)
+  out$Party <- factor(out$Party,labels=parties)
+  return(out)
 }
-getPV <- function(m) {
-   p.D <- collectPredictedValues(m[['s.D']])
-   p.R <- collectPredictedValues(m[['s.R']])
-   p.No <- as.data.frame(rbind(p.R[,c(6,7)],p.D[,c(6,7)]))
-   p.No$group  <- rownames(p.No)
-   names(p.No) <- c('Yes','No','Group')
-   p.No$Amount <- as.numeric(gsub("^P.*=","",p.No$Group,perl=T))
-   p.No$Party  <- gsub("(Party=|,.*)","",p.No$Group,perl=T)
-   p.No <- p.No[,c('Party','Amount','Yes','No')]   
-   return(p.No)
-}
-getEV <- function(m) {
-   p.D <- collectPredictedValues(m[['s.D']])
-   p.R <- collectPredictedValues(m[['s.R']])
-   p.No <- as.data.frame(rbind(p.R[,c(1,2)],p.D[,c(1,2)]))
-   p.No$group  <- rownames(p.No)
-   names(p.No) <- c('Mean','SD','Group')
-   p.No$Amount <- as.numeric(gsub("^P.*=","",p.No$Group,perl=T))
-   p.No$Party  <- gsub("(Party=|,.*)","",p.No$Group,perl=T)
-   p.No <- p.No[,c('Party','Amount','Mean','SD')]   
-   return(p.No)
-}
-# Predicted P(No) graph
-drawPPic <- function(m,mname) {
-   p.No <- getPV(m)
-   pic <- ggplot(data=p.No, aes(x=Amount, y=No, group=Party, color=Party)) + geom_line() + xlab('Funding from security and defense industry') + ylab('Predicted P(votes No)') + ggtitle(paste('Predicted probability of a No vote by party,',mname,sep=' '))
-   pic <- pic + scale_colour_manual(values=c("blue", "red"))    
-   return(pic)
-}
+  
 # Expected P(No) graph
 drawEPic <- function(m,mname) {
-   p.No <- getEV(m)
-   pic <- ggplot(data=p.No, aes(x=Amount, y=Mean, group=Party, color=Party)) + geom_line() + xlab('Funding from security and defense industry') + ylab('Expected P(votes No)') + ggtitle(paste('Expected probability of a No vote by party,',mname,sep=' '))
-   pic <- pic + scale_colour_manual(values=c("blue", "red"))   
+   pic <- ggplot(data=m, aes(x=Amount, y=Median, group=Party)) + 
+     geom_ribbon(aes(ymin=Low,ymax=High,fill=Party),alpha=.2) +
+     geom_line(aes(color=Party)) + xlab('Funding from security and defense industry') + 
+     ylab('Expected P(votes No)') + 
+     ggtitle(paste('Expected probability of a No vote by party,',mname,sep=' '))
+   pic <- pic + scale_colour_manual(values=myRedBlue)
    return(pic)
 }
-ppic1 <- drawPPic(m1,'Model 1')
-ppic2 <- drawPPic(m2,'Model 2')
-epic1 <- drawEPic(m1,'Model 1')
-epic2 <- drawEPic(m2,'Model 2')
-p1.No <- getPV(m1)
-p2.No <- getPV(m2)
 
-# Now try and draw somehow vertical confidence 
-# intervals. plot.ci() in latest Zelig bugged.
-# Step 1. Get the 95CI for a No by amount, by 
-# party, for a given model.
-getCIData <- function(m) {
-   p.D <- collectPredictedValues(m[['s.D']])
-   p.R <- collectPredictedValues(m[['s.R']])
-   p.No <- as.data.frame(rbind(p.R[,c(4,5)],p.D[,c(4,5)]))
-   p.No$group  <- rownames(p.No)
-   names(p.No) <- c('Lower','Upper','Group')
-   p.No$Amount <- as.numeric(gsub("^P.*=","",p.No$Group,perl=T))
-   p.No$Party  <- gsub("(Party=|,.*)","",p.No$Group,perl=T)
-   p.No <- p.No[,c('Party','Amount','Lower','Upper')]   
-   return(p.No)
-}
-# Then put it into long format (make it tidy for ggplot2 to handle)
-makeItLong <- function(p) {
-   p.long <- reshape(p, direction="long", varying=c('Lower','Upper'), v.names="Value", idvar=c("Party","Amount"), timevar="CI", times=c('Lower','Upper'))
-   p.long.sorted <- p.long[order(p.long$Party,p.long$Amount,p.long$CI),]
-   return(as.data.frame(p.long.sorted))
-}
-ci.m1 <- makeItLong(getCIData(m1))
-ci.m2 <- makeItLong(getCIData(m2))
-getCIplot <- function(df,mname) {
-   foo <- df
-   foo$Grouping <- df$Amount+as.numeric(as.factor(df$Party))
-   p <- ggplot(data=foo, aes(x=Amount, y=Value, group=Amount, colour=Party)) + geom_point() + geom_line(aes(group = Grouping))   
-   p <- p + xlab('Funding from security and defense industry') + ylab('Expected P(votes No)') + ggtitle(paste('95% confidence intervals for the expected \n probabilities of a No vote by party,',mname,sep=' '))
-   p <- p + scale_colour_manual(values=c("blue", "red"))
-   return(p)
-}
-ci.plot.m1 <- getCIplot(ci.m1,'Model 1')
-ci.plot.m2 <- getCIplot(ci.m2,'Model 2')
+m1r   <- getRibbon(m1)
+m2r   <- getRibbon(m2)
+epic1 <- drawEPic(m1r,'Model 1')
+epic2 <- drawEPic(m2r,'Model 2')
+
+# Now add vertical bars to the plots where the
+# amount is enough to cross the .5 probability
+# threshold for each party.
+m1pair <- (subset(m1r,Median>.5 & Median<.53) %>% group_by(Party) %>% slice(which.min(Median)))
+m2pair <- (subset(m2r,Median>.5 & Median<.53) %>% group_by(Party) %>% slice(which.min(Median)))
+pic1   <- epic1 + geom_segment(data=m1pair,
+                               aes(x = Amount,
+                                   y = Low,
+                                   xend = Amount,
+                                   yend = High),
+                               color=myRedBlue,
+                               size=2) +
+                  geom_hline(aes(yintercept=.5),alpha=.2)
+pic2   <- epic2 + geom_segment(data=m2pair,
+                               aes(x = Amount,
+                                   y = Low,
+                                   xend = Amount,
+                                   yend = High),
+                               color=myRedBlue,
+                               size=2) +
+                  geom_hline(aes(yintercept=.5),alpha=.2)
+
